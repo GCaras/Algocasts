b3601bacba07aee7811458b7d3062c8ffe34569a
GCaras <gcaras0@gmail.com>
2019-12-16 17:36:37 -0500

    completed string reversal excercise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// Solution 1
+// function reverse(str) {
+//     const arr = str.split('');
+//     arr.reverse();
+//     return arr.join('');
+// }
+
+
+// Solution 1 Refactor
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+//Solution 2
+// function reverse(str) {
+//     var reversed = '';
+
+//     //new for loop syntax
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+
+//     return reversed
+// }
+
+//Solution 3
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) =>  char + rev, '');
+// }
 
 module.exports = reverse;
