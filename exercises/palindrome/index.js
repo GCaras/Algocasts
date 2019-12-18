8b82add445533cedd0946c07ab6220432a9005d2
GCaras <gcaras0@gmail.com>
2019-12-17 22:14:08 -0500

    palindrome solution 1

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,13 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    const pal = str.split('').reverse().join('');
+    if (str === pal) {
+        return true
+    } else {
+        return false
+    }
+}
 
 module.exports = palindrome;
