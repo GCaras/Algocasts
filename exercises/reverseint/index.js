39a7dfcd1e566cda090ff7885b1bed649ea4c427
GCaras <gcaras0@gmail.com>
2019-12-18 22:19:16 -0500

    Reverse integer solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//Solution 1
+function reverseInt(n) {
+    
+    //Stringify number and assign to variable
+    const intRev = n.toString().split('').reverse().join('');
+
+    //convert to negative integer if n is less than 0
+    if (n < 0) {
+        return parseInt(intRev) * -1;
+    } else {
+        return parseInt(intRev);
+    }
+}
 
 module.exports = reverseInt;
