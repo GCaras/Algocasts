782f7238fbd7814c73d553e0cbc67b4db9776f9e
GCaras <gcaras0@gmail.com>
2020-01-09 11:05:27 -0500

    steps recursive solution with ternary operator

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -18,23 +18,45 @@
 //       '####'
 
 // Personal Solution:
-function steps(n) {
-    //loop through each 'row' for 'column' comparison and console.log
-    for (let i = 0; i < n; i++) {
-        let stair = '';
 
-        //loop through each 'column' and add a '#' if less than or equal 
-        //to current 'row' or ' ' if  greater than current 'row'
-        for (let j = 0; i < n; j++) {
-            if (j <= i) {
-                stair += '#';
-            } else {
-                stair += ' ';
-            }
-        }
+// function steps(n) {
+//     //loop through each 'row' for 'column' comparison and console.log
+//     for (let i = 0; i < n; i++) {
+//         let stair = '';
 
-        console.log(stair);
+//         //loop through each 'column' and add a '#' if less than or equal 
+//         //to current 'row' or ' ' if  greater than current 'row'
+//         for (let j = 0; i < n; j++) {
+//             if (j <= i) {
+//                 stair += '#';
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+
+//         console.log(stair);
+//     }
+// }
+
+
+// Recursive solution with ternary operator:
+
+function steps(n, row = 0, stair = '') {
+    //exit statement for recursion
+    if ( n === row ) {
+        return;
     }
+
+    //check if string is equal to n and return console.log statement
+    if ( n === stair.length){
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    //add '#' if row is equal to or less than row or ' ' if not 
+    const add = stair.length <= row ? '#' : ' ';
+
+    steps(n, row, stair + add);
 }
 
 module.exports = steps;
