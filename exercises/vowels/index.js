30856bb9e016e638980193d14415684d032abcc1
GCaras <gcaras0@gmail.com>
2020-01-13 19:55:41 -0500

    personal solution for vowel search

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+//Personal solution:
+
+function vowels(str) {
+    let count = 0;
+
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    for ( let char of str.toLowerCase()) {
+        if (vowels.includes(char)) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
 
 module.exports = vowels;
