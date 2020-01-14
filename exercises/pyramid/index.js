5e0114397fc3d5b37471c210568589c071404df4
GCaras <gcaras0@gmail.com>
2020-01-13 19:36:14 -0500

    pyramids excercise solution 1

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,24 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+// Solution 1:
+
+function pyramid(n) {
+    const midPoint = Math.floor((2 * n - 1) / 2);
+
+    for (let row = 0; row < n; row++) {
+        let level = '';
+
+        for (let col = 0; col < 2 * n - 1; col++) {
+            if (midPoint - row <= col && midPoint + row >= col) {
+                level += '#';
+            } else {
+                level += ' ';
+            }
+        }
+
+        console.log(level);
+    }
+}
 
 module.exports = pyramid;
